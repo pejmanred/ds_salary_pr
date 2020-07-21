0d4b2a89a3420cebe9d242e843828f1aa8a1702d
pejmanred <praisi15@gmail.com>
2020-07-21 18:47:27 -0500

    uploaded scraper and run code

---
+++ Ccode.py
@@ -0,0 +1,2 @@
+import pandas as pd
+DataFr=pd.read_csv('glassdoor_jobs.csv',index_col=False)
---
+++ ruun.py
@@ -0,0 +1,8 @@
+import GLDO_scraper as gs
+import pandas as pd
+path="C:/Users/Nadia/Documents/ds_salary_pr/chromedriver"
+
+df4=gs.get_jobs('data scientist',1000,False,path,6)
+
+
+
